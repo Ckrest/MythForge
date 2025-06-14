e7189b7db535f498b81e432a2f87293a2de1738a
Ckrest <Ckrest11@gmail.com>
2025-06-14 03:04:43 -0500

    adding server logging prep

--- mythforge/model.py
+++ mythforge/model.py
@@ -166,7 +166,7 @@ def call_llm(system_prompt: str, user_prompt: str, **overrides):
     params.update(overrides)
 
     background = params.pop("background", False)
-    stream = params.pop("stream", False)
+    stream = params.pop("stream", True)
 
     cmd = model_launch(user_prompt, background=background, **params)
 
